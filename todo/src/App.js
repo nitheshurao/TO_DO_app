495b620ec30e13f8e811961874f53875152f4c28
Nithesh <nitheshurao@gmail.com>
2021-08-02 23:04:43 +0530

    .

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,12 +6,26 @@ import Pages from './Componts2/Pages';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Todo1 /> */}
-      <Pages />
-      <Todo />
+    <div>
+      <h2> Taks One</h2>
+      <div className="App">
+        {/* <Todo1 /> */}
+
+        <Pages />
+
+
+      </div>
+      <h2 >Task Two</h2>
+      <div className="App">
+        {/* <Todo1 /> */}
+
+
+
+        <Todo />
+      </div>
 
     </div>
+
   );
 }
 

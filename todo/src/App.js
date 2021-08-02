c9cb88da0b263dc8edc0f7e29fc04cf33a2d4128
Nithesh <nitheshurao@gmail.com>
2021-08-02 22:18:09 +0530

    User-pages
    Use this page https://reqres.in/api/users?page=2  and display a list of users.
    This api works page wise i.e page1, 2, etc. So there should be a similar paging in our app.  Important to not that the number of pages are not fixed to 2, but its dynamic so paging should be dynamic as well
    Use any ui library to display a list of user and having a paging option with it.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Todo from './Complonets/Todo';
+import Pages from './Componts2/Pages';
 // import Todo1 from './Complonets/Todo1';
 
 function App() {
   return (
     <div className="App">
       {/* <Todo1 /> */}
+      <Pages />
       <Todo />
 
     </div>
